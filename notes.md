9de695397638864a38c23a25b1c839161203f4fe
Akshit Tyagi <tyagiakshit547@gmail.com>
2026-01-02 23:27:01 +0530

    exploring get, post, delete

---
+++ notes.md
@@ -0,0 +1,4 @@
+browser is the wrost way to test your API, your route and your backend code
+browser is not a good way
+so we their is a software postman
+                            
--- src/app.js
+++ src/app.js
@@ -2,22 +2,41 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//     res.send("HELLO HELLO from dasboard !!")
+// app.use("/user", (req, res) => {     // on typin this will run /user/.... (anything) eg /user/Akscc
+//     res.send("HAHAHAHAHA")           // but on work on         /user.... (anything) abcoz ab string hi change hogi na eg /userAKS
 // })
 
-app.use("/test", (req, res) => {
-    res.send("HELLO from UP")
+app.get("/user", (req, res) => {
+    res.send({firstName : "Akshit", secondName : "Tyagi"})
 })
 
-app.use("/work", (req, res) => {
-    res.send("HELLO from college")
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved to database")
 })
 
-app.use( (req, res) => {
-    res.send("HELLO HELLO")
+app.delete("/user", (req, res) => {
+    res.send("Delete successfully")
 })
 
-app.listen(300, () => {
+// app.use("/test/2", (req, res) => {
+//     res.send("HELLO from UP 2")
+// })
+
+app.use("/test", (req, res) => {             // knows as routes
+    res.send("HELLO from UP 1")
+})
+
+// app.use("/work", (req, res) => {
+//     res.send("HELLO from college")
+// })
+
+// app.use( (req, res) => {                            
+//     res.send("HELLO HELLO u are on dasboard")
+// })
+
+app.listen(7777, () => {
     console.log("Server is succesfully listening on port 300");
 });
+
+
+
