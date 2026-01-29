5871c9fb808005490a05625b90e0e656a644dca9
Akshit Tyagi <tyagiakshit547@gmail.com>
2026-01-29 19:15:52 +0530

    get API

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://namastedev:ECXK5j0d4OuHZhs6@namastenode.rc211ms.mongodb.net/devTinder"
+    // "mongodb+srv://namastedev:ECXK5j0d4OuHZhs6@namastenode.rc211ms.mongodb.net/devTinder"
+    "mongodb+srv://akshit_tyagi:AkshitDATABASE@namastenode.pwreiex.mongodb.net/devTinder"
   );
 };
 
